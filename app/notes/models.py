26b5aaa5da10ad3aacb6eb7e0aae6e02a8492cbd
Vickysahu12 <bikisahu161@gmail.com>
2025-09-29 12:26:52 +0530

    made the user model

---
+++ app/notes/models.py
@@ -0,0 +1,11 @@
+from sqlalchemy.orm import Mapped,mapped_column
+from sqlalchemy import String
+from app.db.base import Base
+
+#User-model:
+class User(Base):
+    __tablename__ = "users"
+
+    id:Mapped[int] = mapped_column(primary_key=True)
+    name:Mapped[str] = mapped_column(String(50),nullable=False)
+    email:Mapped[str] = mapped_column(String,nullable=False,unique=True)
